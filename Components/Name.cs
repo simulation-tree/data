4a3e1b99ad76bb3941ee2a7923aad18d3c235a3a
Phill <philldamaskin@gmail.com>
2024-07-05 00:03:26 -0500

    Create Name.cs

---
+++ Components/Name.cs
@@ -0,0 +1,14 @@
+﻿using Unmanaged;
+
+namespace Data.Components
+{
+    public readonly struct Name
+    {
+        public readonly FixedString value;
+
+        public Name(FixedString value)
+        {
+            this.value = value;
+        }
+    }
+}
