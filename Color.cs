37ab3481a43f5d53914edf848d7ab50fb9f426e0
Phill <philldamaskin@gmail.com>
2024-08-10 13:33:42 -0400

    polish `Color`

--- Color.cs
+++ Color.cs
@@ -1,10 +1,31 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Numerics;
 
 namespace Data
 {
     public struct Color
     {
+        public static readonly Color Black = new(0, 0, 0, 1);
+        public static readonly Color White = new(1, 1, 1, 1);
+        public static readonly Color Grey = new(0.5f, 0.5f, 0.5f, 1);
+
+        public static readonly Color Red = new(1, 0, 0, 1);
+        public static readonly Color Green = new(0, 1, 0, 1);
+        public static readonly Color DeepBlue = new(0, 0, 1, 1);
+
+        public static readonly Color Yellow = new(1, 1, 0, 1);
+        public static readonly Color Cyan = new(0, 1, 1, 1);
+        public static readonly Color Magenta = new(1, 0, 1, 1);
+
+        public static readonly Color Orange = new(1, 0.5f, 0, 1);
+        public static readonly Color Chartreuse = new(0, 1, 0.5f, 1);
+        public static readonly Color SpringGreen = new(0, 0.5f, 1, 1);
+
+        public static readonly Color Violet = new(0.5f, 0, 1, 1);
+        public static readonly Color Rose = new(1, 0, 0.5f, 1);
+        public static readonly Color SkyBlue = new(0, 0.5f, 1, 1);
+
         private Vector4 value;
 
         public float Hue
@@ -64,10 +85,7 @@ namespace Data
 
         public float Value
         {
-            readonly get
-            {
-                return Math.Max(value.X, Math.Max(value.Y, value.Z));
-            }
+            readonly get => Math.Max(value.X, Math.Max(value.Y, value.Z));
             set
             {
                 this = FromHSV(Hue, Saturation, value, Alpha);
@@ -76,46 +94,34 @@ namespace Data
 
         public float Alpha
         {
-            readonly get
-            {
-                return value.W;
-            }
+            readonly get => value.W;
             set
             {
                 this.value = new(this.value.X, this.value.Y, this.value.Z, value);
             }
         }
 
-        public float Red
+        public float RedChannel
         {
-            readonly get
-            {
-                return value.X;
-            }
+            readonly get => value.X;
             set
             {
                 this.value = new(value, this.value.Y, this.value.Z, this.value.W);
             }
         }
 
-        public float Green
+        public float GreenChannel
         {
-            readonly get
-            {
-                return value.Y;
-            }
+            readonly get => value.Y;
             set
             {
                 this.value = new(this.value.X, value, this.value.Z, this.value.W);
             }
         }
 
-        public float Blue
+        public float BlueChannel
         {
-            readonly get
-            {
-                return value.Z;
-            }
+            readonly get => value.Z;
             set
             {
                 this.value = new(this.value.X, this.value.Y, value, this.value.W);
@@ -134,25 +140,30 @@ namespace Data
 
         public static Color FromHSV(float hue, float saturation, float value, float alpha = 1f)
         {
+            ThrowIfOutOfRange(hue);
             byte pie = (byte)(hue * 6);
             float f = hue * 6 - pie;
             float p = value * (1 - saturation);
             float q = value * (1 - f * saturation);
             float t = value * (1 - (1 - f) * saturation);
-            switch (pie)
+            return pie switch
             {
-                case 0:
-                    return new Color(value, t, p, alpha);
-                case 1:
-                    return new Color(q, value, p, alpha);
-                case 2:
-                    return new Color(p, value, t, alpha);
-                case 3:
-                    return new Color(p, q, value, alpha);
-                case 4:
-                    return new Color(t, p, value, alpha);
-                default:
-                    return new Color(value, p, q, alpha);
+                0 => new Color(value, t, p, alpha),
+                1 => new Color(q, value, p, alpha),
+                2 => new Color(p, value, t, alpha),
+                3 => new Color(p, q, value, alpha),
+                4 => new Color(t, p, value, alpha),
+                5 => new Color(value, p, q, alpha),
+                _ => default
+            };
+        }
+
+        [Conditional("DEBUG")]
+        private static void ThrowIfOutOfRange(float hue)
+        {
+            if (hue < 0 || hue > 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(hue), hue, "Hue must be between contained within the 0-1 range");
             }
         }
     }
