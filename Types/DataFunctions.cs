196c3db87ffb215e510df5d25f537c7f608ba32a
Phill <philldamaskin@gmail.com>
2024-08-09 22:08:21 -0400

    remove `unmanaged` restriction here

--- Types/DataFunctions.cs
+++ Types/DataFunctions.cs
@@ -5,17 +5,17 @@ using Unmanaged;
 
 public static class DataFunctions
 {
-    public static Span<byte> GetBytes<T>(this T data) where T : unmanaged, IData
+    public static Span<byte> GetBytes<T>(this T data) where T : IData
     {
         return data.GetList<T, byte>().AsSpan();
     }
 
-    public static FixedString GetAddress<T>(this T data) where T : unmanaged, IData
+    public static FixedString GetAddress<T>(this T data) where T : IData
     {
         return data.GetComponent<T, IsData>().address;
     }
 
-    public static void Clear<T>(this T data) where T : unmanaged, IData
+    public static void Clear<T>(this T data) where T : IData
     {
         data.GetList<T, byte>().Clear();
     }
@@ -23,7 +23,7 @@ public static class DataFunctions
     /// <summary>
     /// Appends the given text as UTF8 formatted bytes.
     /// </summary>
-    public static void Write<T>(this T data, ReadOnlySpan<char> text) where T : unmanaged, IData
+    public static void Write<T>(this T data, ReadOnlySpan<char> text) where T : IData
     {
         using BinaryWriter writer = BinaryWriter.Create();
         writer.WriteUTF8Span(text);
@@ -33,7 +33,7 @@ public static class DataFunctions
     /// <summary>
     /// Appends the given bytes.
     /// </summary>
-    public static void Write<T>(this T data, ReadOnlySpan<byte> bytes) where T : unmanaged, IData
+    public static void Write<T>(this T data, ReadOnlySpan<byte> bytes) where T : IData
     {
         data.GetList<T, byte>().AddRange(bytes);
     }
