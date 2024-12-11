f0e1adff28c26782196243c07a1fe78f9af53b5d
Phill <philldamaskin@gmail.com>
2024-12-11 12:24:15 -0500

    Create NameExtensions.cs

---
+++ source/Extensions/NameExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Data.Components;
+using Unmanaged;
+using Worlds;
+
+namespace Data
+{
+    public static class NameExtensions
+    {
+        public static ref FixedString GetName<T>(this T entity) where T : unmanaged, IEntity
+        {
+            return ref entity.AsEntity().GetComponent<Name>().value;
+        }
+    }
+}
