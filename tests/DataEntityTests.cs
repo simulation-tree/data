9527d5c9d7e3e576a491a420f9492957c2bbbcc5
Phill <philldamaskin@gmail.com>
2025-01-24 22:07:01 -0500

    reference generators

---
+++ tests/DataEntityTests.cs
@@ -0,0 +1,23 @@
+﻿using Unmanaged;
+using Worlds;
+
+namespace Data.Tests
+{
+    public class DataEntityTests : DataTests
+    {
+        [Test]
+        public void LoadDataFromEntity()
+        {
+            using World world = CreateWorld();
+            DataSource data = new(world, "hello", "data");
+
+            Assert.That(data.Address.ToString(), Is.EqualTo("hello"));
+
+            using BinaryReader reader = new(data.Bytes);
+            USpan<char> buffer = stackalloc char[128];
+            uint length = reader.ReadUTF8Span(buffer);
+
+            Assert.That(buffer.Slice(0, length).ToString(), Is.EqualTo("data"));
+        }
+    }
+}
--- tests/DataTests.cs
+++ tests/DataTests.cs
@@ -1,44 +1,26 @@
-﻿using Data.Components;
-using Types;
-using Unmanaged;
+﻿using Types;
 using Unmanaged.Tests;
 using Worlds;
 
 namespace Data.Tests
 {
-    public class DataTests : UnmanagedTests
+    public abstract class DataTests : UnmanagedTests
     {
         static DataTests()
         {
-            TypeLayout.Register<IsDataRequest>();
-            TypeLayout.Register<IsDataSource>();
-            TypeLayout.Register<IsData>();
-            TypeLayout.Register<BinaryData>();
+            TypeRegistry.Load<Data.TypeBank>();
         }
 
-        [Test]
-        public void LoadDataFromEntity()
+        public static World CreateWorld()
         {
-            using World world = CreateWorld();
-            DataSource data = new(world, "hello", "data");
-
-            Assert.That(data.Address.ToString(), Is.EqualTo("hello"));
-
-            using BinaryReader reader = new(data.Bytes);
-            USpan<char> buffer = stackalloc char[128];
-            uint length = reader.ReadUTF8Span(buffer);
-
-            Assert.That(buffer.Slice(0, length).ToString(), Is.EqualTo("data"));
+            return new(CreateSchema());
         }
 
-        private static World CreateWorld()
+        public static Schema CreateSchema()
         {
             Schema schema = new();
-            schema.RegisterComponent<IsDataRequest>();
-            schema.RegisterComponent<IsDataSource>();
-            schema.RegisterComponent<IsData>();
-            schema.RegisterArrayElement<BinaryData>();
-            return new(schema);
+            schema.Load<Data.SchemaBank>();
+            return schema;
         }
     }
 }
