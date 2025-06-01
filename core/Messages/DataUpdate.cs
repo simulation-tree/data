e332932c47311c0b62a0c0b61b9d423151a4b8c9
Phill <philldamaskin@gmail.com>
2025-06-01 12:24:23 -0400

    add update message

---
+++ core/Messages/DataUpdate.cs
@@ -0,0 +1,21 @@
+﻿namespace Data.Messages
+{
+    /// <summary>
+    /// Message for updating only data systems.
+    /// </summary>
+    public readonly struct DataUpdate
+    {
+        /// <summary>
+        /// Time passed.
+        /// </summary>
+        public readonly double deltaTime;
+
+        /// <summary>
+        /// Initializes the message.
+        /// </summary>
+        public DataUpdate(double deltaTime)
+        {
+            this.deltaTime = deltaTime;
+        }
+    }
+}
